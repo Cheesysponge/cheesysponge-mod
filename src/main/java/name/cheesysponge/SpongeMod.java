9991583571a5d875191140606a214018d8625db9
Samik <ohnoalieninvasion@gmail.com>
2023-07-13 16:33:09 -0700

    adding first commit

--- src/main/java/name/cheesysponge/SpongeMod.java
+++ src/main/java/name/cheesysponge/SpongeMod.java
@@ -1,5 +1,6 @@
 package name.cheesysponge;
 
+import name.cheesysponge.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -9,14 +10,13 @@ public class 	SpongeMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final String MOD_ID = "cheesysponge";
     public static final Logger LOGGER = LoggerFactory.getLogger("cheesysponge");
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		ModItems.registerModItems();
 
-		LOGGER.info("Hello Fabric world!");
+
 	}
 }
--- src/main/java/name/cheesysponge/item/ModItems.java
+++ src/main/java/name/cheesysponge/item/ModItems.java
@@ -1,4 +1,25 @@
 package name.cheesysponge.item;
 
+import name.cheesysponge.SpongeMod;
+import net.fabricmc.fabric.api.item.v1.CustomDamageHandler;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.FoodComponent;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+
+import net.minecraft.util.Rarity;
+import net.minecraft.util.registry.Registry;
+
+
 public class ModItems {
+    public static final Item SPONGE_INGOT = registerItem("sponge_ingot", new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+    public static final Item CHEESY_SPONGE = registerItem("cheesy_sponge", new Item(new FabricItemSettings().rarity(Rarity.UNCOMMON).fireproof().group(ItemGroup.MISC).maxCount(12)));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(SpongeMod.MOD_ID, name), item);
+    }
+    public static void registerModItems() {
+        SpongeMod.LOGGER.info("Registering Mod Items for " + SpongeMod.MOD_ID);
+    }
 }
