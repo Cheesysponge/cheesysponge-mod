8052a3034a7ccdb3945c13eb12aa1f5d6792a8e0
Samik <ohnoalieninvasion@gmail.com>
2023-07-13 17:04:43 -0700

    changed comment

--- src/main/java/name/cheesysponge/SpongeMod.java
+++ src/main/java/name/cheesysponge/SpongeMod.java
@@ -9,7 +9,7 @@ import org.slf4j.LoggerFactory;
 public class 	SpongeMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
+	// That way, it's clear which mod wrote info, warnings, and errors. W
 	public static final String MOD_ID = "cheesysponge";
     public static final Logger LOGGER = LoggerFactory.getLogger("cheesysponge");
 
